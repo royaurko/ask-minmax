1b4539a9179f26f0126e315b2f89b428a18935df
Aurko Roy <roy.aurko@gmail.com>
2015-05-17 16:28:39 -0400

    added setup.py, changed dir structure, bugs
    Former-commit-id: 34e65f9fe42a8d3eb3345318f40ba9f55e2ef9ee

---
+++ src/askminmax/training.py
@@ -0,0 +1,34 @@
+# Contains functions responsible for training
+
+import helper
+import problems
+import sepquestions
+
+
+def train(db):
+    '''Query number of times to train, then call training'''
+    while True:
+        try:
+            n = int(raw_input('Number of times to train?'))
+            break
+        except ValueError:
+            helper.errornumber()
+    training(n, db)
+
+
+def training(n, db):
+    '''n is the number of trials, q is the set of questions known till now'''
+    for i in range(n):
+        problem = problems.sampleproblem(db, 'prior')
+        print problem['name']
+        while True:
+            try:
+                flag = int(raw_input('Is this the correct problem (0/1)? '))
+                break
+            except ValueError:
+                helper.erroronezero()
+        if flag:
+            problems.increasecount(db, problem)
+            continue
+        else:
+            sepquestions.separatingquestion(db, problem)
