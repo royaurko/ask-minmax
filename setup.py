ec677e0d776eb2029cb66aeab63f912caafd0c26
Aurko Roy <roy.aurko@gmail.com>
2015-05-17 16:44:44 -0400

    added setup.py
    Former-commit-id: 23ed19056ffff2b9bce8a30f776abb4e09eca555

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="askminmax",
+    version="1.0.0",
+    author="Aurko Roy",
+    author_email="aurko@gatech.edu",
+    description=("A Bayesian expert system for optimization problems "),
+    license="BSD",
+    keywords="expert system",
+    url="https://github.com/royaurko/ask-minmax",
+    packages=['askminmax'],
+    package_dir={'askminmax': 'src/askminmax'},
+    package_data={'askminmax': ['src/askminmax/database']},
+    long_description=read('Readme.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
